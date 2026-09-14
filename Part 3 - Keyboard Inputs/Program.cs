6b9f5ab5f0f3d27736aacb92b9c1cd8d63fe4454
362568016 <362568016@2260I-L29-025>
2026-09-14 13:18:33 -0400

    Add project files.

---
+++ Part 3 - Keyboard Inputs/Program.cs
@@ -0,0 +1,20 @@
+﻿string name;
+Console.WriteLine("Greeting!, Whats your name?");
+name = Console.ReadLine();
+Console.WriteLine($"Great! {name} Now, whats your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Awsome {name}, your {age}! Lastly, type in the current year.");
+int year = Convert.ToInt32(Console.ReadLine());
+int bornYear = (bornYear = (year) - (age));
+Console.WriteLine($"Wow {year} That must mean you were born in {bornYear}!");
+
+
+Console.WriteLine($"Alright {name}, Let's do some math! Start by typing in a number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Nice {name}! Now number 2");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Okay, Just one last number.");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int forthNumber = (forthNumber = (firstNumber) + (secondNumber) + (thirdNumber));
+Console.WriteLine($"Sweet! So if I add these numbers together, than you will get {forthNumber}!");
+
